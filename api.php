e43339ffa198fb29e55470a4f1b163c61fd42f99
Gary Wang <wzc782970009@gmail.com>
2017-12-05 00:29:32 +0800

    if empty, not add a slash to the end

--- api.php
+++ api.php
@@ -48,7 +48,7 @@
 		case "getfilelist":
 			$requestFolderStr = "";
 			if(isset($_POST['folder'])) $requestFolderStr = $_POST['folder'];
-			if(substr($requestFolderStr, -1) != '/') $requestFolderStr.='/';
+			if(substr($requestFolderStr, -1) != '/' && strlen($requestFolderStr) > 1) $requestFolderStr.='/';
 			$actualSongFolder = null;
 			if(is_dir($songFolderPath."/".urldecode($requestFolderStr))) {
 				$actualSongFolder = $songFolderPath."/".urldecode($requestFolderStr);
